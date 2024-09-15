66edf6aede407129705a6fdf8afe9ca2f2b83553
SimksS <kelven.souza00@gmail.com>
2024-09-15 13:02:12 -0300

    commit

--- .lintstagedrc.js
+++
@@ -1,7 +0,0 @@
-export default {
-  // Lint & Prettify TS Files
-  '*.{js,jsx,ts,tsx}': (filenames) => [
-    `prettier --write ${filenames.join(' ')}`,
-    `npm run lint --fix . ${filenames.join(' --file')}`
-  ]
-}
